3c04897651090603b898d09e4ed8312983993d4e
shanidha p p <shanidhapp@outlook.com>
2022-05-26 00:30:07 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/linkdn_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.linkdn_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/user_state.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import './jobs/jobs_screen.dart';
+
+import 'auth/login.dart';
+
+class UserState extends StatelessWidget {
+  const UserState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.data == null) {
+            print('user is not logged in yet');
+            return Login();
+          } else if (userSnapshot.hasData) {
+            print('user is already logged in');
+            return JobScreen();
+          } else if (userSnapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                child: Text('An error has been occurred. Try again later'),
+              ),
+            );
+          } else if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          return Scaffold(
+            body: Center(
+              child: Text('Something went wrong'),
+            ),
+          );
+        });
+  }
+}
